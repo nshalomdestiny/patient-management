03fff45383aac0bef406cd115d35ea3b2ab1a4fb
nshalomdestiny <137232309+nshalomdestiny@users.noreply.github.com>
2025-09-17 20:33:06 -0400

    Injected Patient Repository into Patient Service

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,21 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setFirstName(patient.getFirstName());
+        patientDTO.setMiddleName(patient.getMiddleName());
+        patientDTO.setLastName(patient.getLastName());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientDTO;
+
+    }
+
+}
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,7 +1,12 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -10,5 +15,13 @@ public class PatientService {
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
 
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        return patients.stream()
+                .map(PatientMapper::toDTO)
+                .toList();
     }
 }
